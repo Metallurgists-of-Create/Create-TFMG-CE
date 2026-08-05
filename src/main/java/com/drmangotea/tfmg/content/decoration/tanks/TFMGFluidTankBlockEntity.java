9d870dd32100ed6b286a4a2c7c4587fe04600730
Shallow Assumptions <mudawiabubaker@gmail.com>
2026-08-05 22:31:46 +0100

    migrate tfmg fluid tank logic out of mixin and into TFMGFluidTankBlockEntity, removed mixin

--- src/main/java/com/drmangotea/tfmg/content/decoration/tanks/TFMGFluidTankBlockEntity.java
+++ src/main/java/com/drmangotea/tfmg/content/decoration/tanks/TFMGFluidTankBlockEntity.java
@@ -1,7 +1,11 @@
 package com.drmangotea.tfmg.content.decoration.tanks;
 
+import com.drmangotea.tfmg.content.decoration.tanks.aluminum.AluminumTankBlock;
+import com.drmangotea.tfmg.content.decoration.tanks.cast_iron.CastIronTankBlock;
 import com.drmangotea.tfmg.mixin.accessor.FluidTankBlockEntityAccessor;
 import com.drmangotea.tfmg.registry.TFMGBlockEntities;
+import com.drmangotea.tfmg.registry.TFMGBlocks;
+import com.simibubi.create.content.fluids.tank.FluidTankBlock;
 import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -9,8 +13,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
-public class TFMGFluidTankBlockEntity extends FluidTankBlockEntity {
+import static java.lang.Math.abs;
 
+public class TFMGFluidTankBlockEntity extends FluidTankBlockEntity {
     public TFMGFluidTankBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -26,4 +31,65 @@ public class TFMGFluidTankBlockEntity extends FluidTankBlockEntity {
                 }
         );
     }
+	
+	@Override
+	public void notifyMultiUpdated() {
+		BlockState state = this.getBlockState();
+		if (AluminumTankBlock.isTank(state)|| CastIronTankBlock.isTank(state)) {
+			state = state.setValue(FluidTankBlock.BOTTOM, getController().getY() == getBlockPos().getY());
+			state = state.setValue(FluidTankBlock.TOP, getController().getY() + height - 1 == getBlockPos().getY());
+			level.setBlock(getBlockPos(), state, 6);
+			
+		}
+	}
+	
+	@Override
+	public void removeController(boolean keepFluids) {
+		if (level.isClientSide)
+			return;
+		
+		BlockState state = getBlockState();
+		if (AluminumTankBlock.isTank(state)|| CastIronTankBlock.isTank(state)) {
+			state = state.setValue(FluidTankBlock.BOTTOM, true);
+			state = state.setValue(FluidTankBlock.TOP, true);
+			state = state.setValue(FluidTankBlock.SHAPE, window ? FluidTankBlock.Shape.WINDOW : FluidTankBlock.Shape.PLAIN);
+			getLevel().setBlock(worldPosition, state, 22);
+		}
+	}
+	
+	@Override
+	public void setWindows(boolean window) {
+		if(getBlockState().is(TFMGBlocks.ALUMINUM_FLUID_TANK.get())||getBlockState().is(TFMGBlocks.CAST_IRON_FLUID_TANK.get())) {
+			this.window = window;
+			for (int yOffset = 0; yOffset < height; yOffset++) {
+				for (int xOffset = 0; xOffset < width; xOffset++) {
+					for (int zOffset = 0; zOffset < width; zOffset++) {
+						
+						BlockPos pos = this.worldPosition.offset(xOffset, yOffset, zOffset);
+						BlockState blockState = level.getBlockState(pos);
+						
+						if (!AluminumTankBlock.isTank(blockState)&&!CastIronTankBlock.isTank(blockState))
+							continue;
+						FluidTankBlock.Shape shape = FluidTankBlock.Shape.PLAIN;
+						if (window) {
+							// SIZE 1: Every tank has a window
+							if (width == 1)
+								shape = FluidTankBlock.Shape.WINDOW;
+							// SIZE 2: Every tank has a corner window
+							if (width == 2)
+								shape = xOffset == 0 ? zOffset == 0 ? FluidTankBlock.Shape.WINDOW_NW : FluidTankBlock.Shape.WINDOW_SW
+									: zOffset == 0 ? FluidTankBlock.Shape.WINDOW_NE : FluidTankBlock.Shape.WINDOW_SE;
+							// SIZE 3: Tanks in the center have a window
+							if (width == 3 && abs(abs(xOffset) - abs(zOffset)) == 1)
+								shape = FluidTankBlock.Shape.WINDOW;
+						}
+						level.setBlock(pos, blockState.setValue(FluidTankBlock.SHAPE, shape), 22);
+						level.getChunkSource()
+							.getLightEngine()
+							.checkBlock(pos);
+					}
+				}
+			}
+		}
+	}
 }
