8593f926b64970c7ad56fdc9ad65243367db3618
Garoot <gwkb2007@gmail.com>
2026-08-08 10:17:56 -0400

    Chemical Vat JEI GUI update thingy with test recipes (DELETE THOSE)

--- src/main/java/com/drmangotea/tfmg/recipes/jei/ChemicalVatCategory.java
+++ src/main/java/com/drmangotea/tfmg/recipes/jei/ChemicalVatCategory.java
@@ -60,7 +60,7 @@ public class ChemicalVatCategory extends CreateRecipeCategory<VatMachineRecipe> 
         }
         /////////////////////////////
 
-        int fluidResultPos = 90;
+        int fluidResultPos = 106;
 
         for (int i = 0; i < recipe.getFluidResults().size(); i++) {
 
@@ -69,7 +69,7 @@ public class ChemicalVatCategory extends CreateRecipeCategory<VatMachineRecipe> 
             fluidResultPos -= 21;
         }
 
-        int itemResultPos = 90;
+        int itemResultPos = 106;
 
         for (int i = 0; i < recipe.getRollableResults().size(); i++) {
             ProcessingOutput output = recipe.getRollableResults().get(i);
@@ -96,12 +96,57 @@ public class ChemicalVatCategory extends CreateRecipeCategory<VatMachineRecipe> 
 
         drawSprites(machines, graphics);
 
+        TFMGGuiTextures.VAT_BAROMETER.render(graphics, 128, 0);
+        if (recipe.pressure==0) {
+            TFMGGuiTextures.VAT_BAROMETER_NEEDLE_OFF.render(graphics,  128+20-13, 0+24);
+        } else {
+
+            TFMGGuiTextures i;
+
+            if (recipe.pressure == -9) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWNINE; }
+            else if (recipe.pressure == 9) {i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHNINE; }
+            else if (recipe.pressure == -8) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWEIGHT; }
+            else if (recipe.pressure == 8) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHEIGHT; }
+            else if (recipe.pressure == -7) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWSEVEN; }
+            else if (recipe.pressure == 7) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHSEVEN; }
+            else if (recipe.pressure == -6) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWSIX; }
+            else if (recipe.pressure == 6) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHSIX; }
+            else if (recipe.pressure == -5) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWFIVE; }
+            else if (recipe.pressure == 5) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHFIVE; }
+            else if (recipe.pressure == -4) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWFOUR; }
+            else if (recipe.pressure == 4) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHFOUR; }
+            else if (recipe.pressure == -3) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWTHREE; }
+            else if (recipe.pressure == 3) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHTHREE; }
+            else if (recipe.pressure == -2) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWTWO; }
+            else if (recipe.pressure == 2) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHTWO; }
+            else if (recipe.pressure == -1) { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_LOWONE; }
+            else { i = TFMGGuiTextures.VAT_BAROMETER_NEEDLE_HIGHONE; }
+            if (recipe.pressure<0) { i.render(graphics, 128 +20 - (i.width-2), 0 + 24-(i.height-2)); }
+            else {i.render(graphics, 128 + 20 , 0 + 24-(i.height-2));}
 
 
-
+        }
         if(recipe.heatLevel!=0) {
-            TFMGGuiTextures.VAT_HEATER.render(graphics, 55 - 10, 109);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf((recipe.heatLevel + 10f) / 10f), 76.0F, 113.0F, 0xFF501C, false);
+            if (recipe.heatLevel>9) {
+                for (int i = 9; i > 0; i--) {
+                    if (recipe.heatLevel >= 9 + i) {
+                        TFMGGuiTextures.VAT_SUPERHEATER.render(graphics, i * 10, 109);
+                    } else {
+                        TFMGGuiTextures.VAT_HEATER.render(graphics, i * 10, 109);
+                    }
+                }
+
+            }else if (recipe.heatLevel>0) {
+                for (int i = recipe.heatLevel; i > 0; i--) {
+                    TFMGGuiTextures.VAT_HEATER.render(graphics, i * 10, 109);
+                }
+            } else {
+                for (int i = -1 * recipe.heatLevel; i > 0; i--) {
+                    TFMGGuiTextures.VAT_FREEZER.render(graphics, i * 10, 109);
+                }
+            }
+
+            //graphics.drawString(Minecraft.getInstance().font, String.valueOf((recipe.heatLevel + 10f) / 10f), 76.0F, 113.0F, 0xFF501C, false);
         }
 
         int pos = 55;
@@ -168,6 +213,7 @@ public class ChemicalVatCategory extends CreateRecipeCategory<VatMachineRecipe> 
             TFMGGuiTextures.GRAPHITE_ELECTRODE.render(graphics, 55 - 4 + 32, 32);
             TFMGGuiTextures.GRAPHITE_ELECTRODE.render(graphics, 55 - 4, 32);
         }
+
     }
 
 }
