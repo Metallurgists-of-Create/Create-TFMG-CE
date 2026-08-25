76e642fea8d7ed414643c0f1fa9611a594bba2b0
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-08-25 07:57:23 -0400

    Fix TFMGCommonConfig extending class

--- src/main/java/com/drmangotea/tfmg/config/TFMGCommonConfig.java
+++ src/main/java/com/drmangotea/tfmg/config/TFMGCommonConfig.java
@@ -1,8 +1,9 @@
 package com.drmangotea.tfmg.config;
 
 
+import net.createmod.catnip.config.ConfigBase;
 
-public class TFMGCommonConfig extends net.createmod.catnip.config.ConfigBase {
+public class TFMGCommonConfig extends ConfigBase {
 
     public final MachineConfig machines = nested(0, MachineConfig::new, "Config options for TFMG's machinery");
     public final DepositConfig worldgen = nested(1, DepositConfig::new, "Worldgen Settings");
