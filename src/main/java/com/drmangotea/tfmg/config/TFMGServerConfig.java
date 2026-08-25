9b44d0368ca4dda66938f169cea4e311ecd63181
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-08-25 07:57:42 -0400

    Remove blank lines in TFMGServerConfig

--- src/main/java/com/drmangotea/tfmg/config/TFMGServerConfig.java
+++ src/main/java/com/drmangotea/tfmg/config/TFMGServerConfig.java
@@ -4,9 +4,6 @@ package com.drmangotea.tfmg.config;
 import net.createmod.catnip.config.ConfigBase;
 
 public class TFMGServerConfig extends ConfigBase {
-
-
-
 	public final TFMGStress stressValues = nested(0, TFMGStress::new, "Fine tune the kinetic stats of individual components");
 	public final TFMGResistivity resistivityValues = nested(0, TFMGResistivity::new, "Fine tune the resistivity stats of individual cable types");
 
