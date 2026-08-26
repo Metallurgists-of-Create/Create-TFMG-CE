df71b7a72c10a314201c51671bb8365899654dc7
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-08-25 21:58:24 -0400

    Change pressureNeedleWobble in UIConfig's comment

--- src/main/java/com/drmangotea/tfmg/config/client/UIConfig.java
+++ src/main/java/com/drmangotea/tfmg/config/client/UIConfig.java
@@ -3,8 +3,7 @@ package com.drmangotea.tfmg.config.client;
 import net.createmod.catnip.config.ConfigBase;
 
 public class UIConfig extends ConfigBase {
-
-    public final ConfigBool pressureNeedleWobble = b(true, "enablePressureNeedleWobble", "Whether the pressure needle wobbles for the aesthetics of super high/low pressure");
+    public final ConfigBool pressureNeedleWobble = b(true, "enablePressureNeedleWobble", "Whether the pressure needle wobbles for the aesthetics of extremely high or low pressure");
 
     @Override
     public String getName() {
