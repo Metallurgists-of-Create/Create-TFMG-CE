65ce09fd255b08a0852c9f065944e83336610fa0
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-09-08 12:43:32 -0400

    Fix datagen from failing due to missing class

--- build.gradle.kts
+++ build.gradle.kts
@@ -146,6 +146,7 @@ dependencies {
     implementation("com.tterrag.registrate:Registrate:${property("registrate_version")}")
 
     compileOnly("dev.wolfieboy09.createliquidfuel:createliquidfuel:${property("minecraft_version")}-${property("clf_reburned_version")}") { isTransitive = false }
+    runtimeOnly("dev.wolfieboy09.createliquidfuel:createliquidfuel:${property("minecraft_version")}-${property("clf_reburned_version")}") { isTransitive = false }
 
     runtimeOnly("me.djtheredstoner:DevAuth-neoforge:1.2.1")
     runtimeOnly("maven.modrinth:lhGA9TYQ:1IiqEQGl") //architectury, for CPG
