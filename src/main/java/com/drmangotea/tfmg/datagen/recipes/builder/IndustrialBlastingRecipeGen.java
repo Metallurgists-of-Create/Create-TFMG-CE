a4e9d6ed6a756572f1d49ff8da3aff62640eff5a
Shallow Assumptions <mudawiabubaker@gmail.com>
2026-09-18 11:45:54 +0100

    clean up blasting recipe gen
    - remove unused type arguments from IndustrialBlastingRecipeGen
    - remove now-unneeded casting

--- src/main/java/com/drmangotea/tfmg/datagen/recipes/builder/IndustrialBlastingRecipeGen.java
+++ src/main/java/com/drmangotea/tfmg/datagen/recipes/builder/IndustrialBlastingRecipeGen.java
@@ -4,24 +4,17 @@ import com.drmangotea.tfmg.recipes.IndustrialBlastingRecipe;
 import com.drmangotea.tfmg.recipes.IndustrialBlastingRecipeParams;
 import com.drmangotea.tfmg.registry.TFMGRecipeTypes;
 import com.simibubi.create.api.data.recipe.ProcessingRecipeGen;
-import com.simibubi.create.content.processing.recipe.ProcessingRecipe;
-import com.simibubi.create.content.processing.recipe.ProcessingRecipeBuilder;
-import com.simibubi.create.content.processing.recipe.ProcessingRecipeParams;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.concurrent.CompletableFuture;
 
-public abstract class IndustrialBlastingRecipeGen<P extends ProcessingRecipeParams, R extends ProcessingRecipe<?, P>, B extends ProcessingRecipeBuilder<P, R, B>> extends ProcessingRecipeGen<IndustrialBlastingRecipeParams, IndustrialBlastingRecipe, IndustrialBlastingRecipe.Builder<IndustrialBlastingRecipe>> {
-
+public abstract class IndustrialBlastingRecipeGen extends ProcessingRecipeGen<IndustrialBlastingRecipeParams, IndustrialBlastingRecipe, IndustrialBlastingRecipe.Builder<IndustrialBlastingRecipe>> {
 
 	public IndustrialBlastingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries,String namespace) {
 		super(output, registries, namespace);
 	}
-
-
-
 
 	@Override
 	protected TFMGRecipeTypes getRecipeType() {
