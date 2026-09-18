ce9f12a1fb52a49aa4e6ae6d74d873795963579a
Shallow Assumptions <mudawiabubaker@gmail.com>
2026-09-18 11:45:54 +0100

    clean up blasting recipe gen
    - remove unused type arguments from IndustrialBlastingRecipeGen
    - remove now-unneeded casting

--- src/main/java/com/drmangotea/tfmg/datagen/recipes/values/tfmg/TFMGIndustrialBlastingRecipeGen.java
+++ src/main/java/com/drmangotea/tfmg/datagen/recipes/values/tfmg/TFMGIndustrialBlastingRecipeGen.java
@@ -1,34 +1,29 @@
 package com.drmangotea.tfmg.datagen.recipes.values.tfmg;
 
-
 import com.drmangotea.tfmg.TFMG;
 import com.drmangotea.tfmg.datagen.recipes.builder.IndustrialBlastingRecipeGen;
-import com.drmangotea.tfmg.recipes.IndustrialBlastingRecipe;
 import com.drmangotea.tfmg.registry.TFMGFluids;
 import com.drmangotea.tfmg.registry.TFMGTags;
 import com.simibubi.create.AllItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
 
+import javax.annotation.Nonnull;
 import java.util.concurrent.CompletableFuture;
 
 import static com.drmangotea.tfmg.datagen.recipes.TFMGRecipeProvider.I.ironDust;
 
-
 public class TFMGIndustrialBlastingRecipeGen extends IndustrialBlastingRecipeGen {
-
-
     GeneratedRecipe
 
-    SILICON = create("silicon", b -> ((IndustrialBlastingRecipe.Builder<IndustrialBlastingRecipe>)b)
+    SILICON = create("silicon", b -> b
             .require(Items.QUARTZ)
             .output(TFMGFluids.LIQUID_SILICON.get(),40)
             .duration(5)
     ),
 
-    STEEL = create("steel", b -> ((IndustrialBlastingRecipe.Builder<IndustrialBlastingRecipe>)b)
+    STEEL_CRUSHED = create("steel", b -> b
             .require(AllItems.CRUSHED_IRON)
             .require(TFMGTags.Items.FLUX.tag)
             .output(TFMGFluids.MOLTEN_STEEL.get(),144)
@@ -37,18 +32,16 @@ public class TFMGIndustrialBlastingRecipeGen extends IndustrialBlastingRecipeGen
             .duration(20)
             .hotAirUsage(20)
     ),
-    STEEL_DOUBLE = create("steel_from_raw_iron", b -> ((IndustrialBlastingRecipe.Builder<IndustrialBlastingRecipe>)b)
+    STEEL_RAW = create("steel_from_raw_iron", b -> b
             .require(Items.RAW_IRON)
-            .require(Ingredient.of(TFMGTags.Items.FLUX.tag))
+            .require(TFMGTags.Items.FLUX.tag)
             .output(TFMGFluids.MOLTEN_STEEL.get(),288)
             .output(TFMGFluids.MOLTEN_SLAG.get(),288)
             .output(TFMGFluids.FURNACE_GAS.get(),200)
             .duration(40)
             .hotAirUsage(40)
     ),
-
-
-    STEEL_DUST = create("steel_from_dust", b -> ((IndustrialBlastingRecipe.Builder<IndustrialBlastingRecipe>)b)
+    STEEL_DUST = create("steel_from_dust", b -> b
             .require(ironDust())
             .require(TFMGTags.Items.FLUX.tag)
             .output(TFMGFluids.MOLTEN_STEEL.get(),144)
@@ -62,13 +55,11 @@ public class TFMGIndustrialBlastingRecipeGen extends IndustrialBlastingRecipeGen
 
     ;
 
-
     public TFMGIndustrialBlastingRecipeGen(PackOutput generator, CompletableFuture<HolderLookup.Provider> registries) {
         super(generator, registries, TFMG.MOD_ID);
     }
 
-
-    @Override
+    @Override @Nonnull
     public String getName() {
         return "TFMG'S Industrial Blasting Recipes";
     }
