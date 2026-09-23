ab00e906d810a95ce4a64b08147a6024873093fb
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-09-23 11:44:16 -0400

    Fix mod metadata into the correct dir

--- build.gradle.kts
+++ build.gradle.kts
@@ -183,7 +183,7 @@ val generateModMetadata by tasks.registering(ProcessResources::class) {
     }
 
     from("src/main/templates")
-    into("build/generated/sources/modMetadata")
+    into("build/resources")
 }
 
 // Include the output of "generateModMetadata" as an input directory for the build.
